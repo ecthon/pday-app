151d0e461a589c20d692759142c442ba40cdcc44
ecthon <ecthon@gmail.com>
2026-08-22 02:19:49 -0300

    style: adjust spacing and structure in Header component for improved layout

--- components/header.tsx
+++ components/header.tsx
@@ -4,11 +4,14 @@ import { LogoutIcon } from '@hugeicons/core-free-icons';
 export function Header() {
   return (
     <header className="flex items-center px-4 w-full justify-between bg-gray-50 py-4 max-w-2xl">
-      <div className="flex w-full items-center gap-4">
+      <div className="flex w-full items-center gap-2">
         <div className="flex size-10 items-center justify-center rounded-full bg-indigo-50">
           <p className="font-semibold">EB</p>
         </div>
-        <h1 className="text-sm font-bold text-gray-800">Olá, Ecthon!</h1>
+        <div className="flex items-start justify-center gap-1">
+          <p className="text-sm font-medium text-gray-600">Olá,</p>
+          <h1 className="text-sm font-bold text-gray-800">Ecthon!</h1>
+        </div>
       </div>
       <button className="flex size-10 shrink-0 items-center justify-center rounded-full bg-white">
         <HugeiconsIcon icon={LogoutIcon} size={20} strokeWidth={2} className="text-zinc-400" />
