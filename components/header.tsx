e2b9c20148fd2b4a8fee6d4103b4cbea519d0faa
ecthon <ecthon@gmail.com>
2026-08-22 02:14:51 -0300

    style: adjust button and icon sizes in Header component for better alignment

--- components/header.tsx
+++ components/header.tsx
@@ -5,12 +5,12 @@ export function Header() {
   return (
     <header className="flex items-center px-4 w-full justify-between bg-gray-50 py-4 max-w-2xl">
       <div className="flex w-full items-center gap-4">
-        <div className="flex size-12 items-center justify-center rounded-full bg-indigo-50">
+        <div className="flex size-10 items-center justify-center rounded-full bg-indigo-50">
           <p className="font-semibold">EB</p>
         </div>
         <h1 className="text-sm font-bold text-gray-800">Olá, Ecthon!</h1>
       </div>
-      <button className="flex size-12 shrink-0 items-center justify-center rounded-full bg-white">
+      <button className="flex size-10 shrink-0 items-center justify-center rounded-full bg-white">
         <HugeiconsIcon icon={LogoutIcon} size={20} strokeWidth={2} className="text-zinc-400" />
       </button>
     </header>
